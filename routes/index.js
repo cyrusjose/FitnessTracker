5c8166ff887a508f11bd4f4e60dfa86cec37310d
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-10 21:54:00 -0700

    Renamed index.html

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  console.log(req);
   res.send("hello world");
-//   res.sendFile(path.join(__dirname, "../public/index.html"))
+  //   res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
 module.exports = router;
-
 
 // module.exports = app => {
 //     app.get("/", (req, res) => {
