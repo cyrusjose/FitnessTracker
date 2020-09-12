4a682507fdd089596173d6a71f1d0bd6e79c924f
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-11 18:31:18 -0700

    Testing routes

--- routes/exercise.js
+++ routes/exercise.js
@@ -1,11 +1,21 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
-// const Exercise = require('../models/exercise')
+const Exercise = require("../models/exercise");
 
 router.get("/", (req, res) => {
-//   res.send("hello world");
-  res.sendFile(path.join(__dirname, "../public/exercise.html"))
+  //   res.send("hello world");
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/workouts", () => {
+  Exercise.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
