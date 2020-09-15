f67157cc2e5c48c5f9f9fa76d4b306ca02c5c3a2
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-15 15:43:21 -0700

    Removed comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,24 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  //   res.send("hello world");
     res.sendFile(path.join(__dirname, "../public/home.html"))
 });
 
 router.get("/exercise", (req, res) => {
-  //   res.send("hello world");
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-  //   res.send("hello world");
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
 
-// module.exports = app => {
-//     app.get("/", (req, res) => {
-//         res.sendFile(path.join(__dirname, "../public/index.html"))
-//     })
-// };
